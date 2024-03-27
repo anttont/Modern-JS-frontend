59c2fc67cc11eabf91ff72bba23cb5b35b390933
anttont <anttoni.tornikoski@gmail.com>
2024-03-28 00:08:37 +0200

    Update header.jsx
    1:08:15

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -3,8 +3,10 @@ import "./header.css"
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className='gpt3__header section__padding' id="home"> 
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laltion ullamco</h1>
+      </div>
     </div>
   )
 }
