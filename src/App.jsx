fdcfb08babc1285c33317ec09bca25d05a2015f5
anttont <anttoni.tornikoski@gmail.com>
2024-03-29 18:06:23 +0200

    Header done
    1:34:08

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,18 @@ import {CTA, Brand, Navbar} from "./components"
 const App = () => {
   return (
     <div className="App">
-      <div className = "gradient__bg"></div>
-      <Navbar/>
-      <Header/>
-      <div>
-        <Brand/>
-        <WhatGPT3 />
-        <Features />
-        <Possibility />
-        <CTA />
-        <Blog />
-        <Footer />
-      </div>
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
   )
 }
 
--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import "./header.css"
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
     <div className='gpt3__header section__padding' id="home"> 
       <div className='gpt3__header-content'>
-        <h1 className='gradient__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laltion ullamco</h1>
+        <h1 className='gradient__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laltion ullamco</p>
+
+      <div className='gpt3__header-content__input'>
+        <input type="email" placeholder='Your email address' />
+        <button type="button"> Get started </button>
+      </div>
+
+      <div className='gpt3__header-content__people'>
+        <img src={people}/>
+        <p>1111 people have requested access</p>
+      </div>
+
+      </div>
+      <div className='gpt3__header-image'>
+        <img src={ai} alt="ai"  />
       </div>
     </div>
   )
