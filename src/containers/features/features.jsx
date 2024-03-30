f983144d800dd1bdfca090aa11cf9d01dfc8ab1b
anttont <anttoni.tornikoski@gmail.com>
2024-03-30 22:41:40 +0200

    Features part added

--- src/containers/features/features.jsx
+++ src/containers/features/features.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import "./features.css"
+import Feature from '../../components/feature/feature'
 
-const Features = () => {
-  return (
-    <div>
-      features
+const featuresData = [
+  {
+    title: 'Improving end distrusts instantly',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
+  },
+  {
+    title: 'Become the tended active',
+    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
+  },
+  {
+    title: 'Message or am nothing',
+    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
+  },
+  {
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush..',
+  },
+]
+
+const Features = () => (
+  
+    <div className='gp3__features section__padding' id='features'>
+      <div className='gpt3__features-heading'>
+        <h1 className='gradient__text'>Welcome to the ultimate course, where you will learn how to take Figma designs, turn them into four modern, responsive, beautifully-designed applications, and get hired a</h1>
+        <p>Use the code JAVASCRIPTMASTERY to save up to 91% on all yearly hosting plans.</p>      
+      </div>
+      <div className='gpt3__features-container'>
+        {featuresData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={item.title + index}/>
+        ))}
+      </div>
     </div>
   )
-}
+
 
 export default Features
