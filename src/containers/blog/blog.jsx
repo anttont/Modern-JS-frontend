cb124ccebfa9a13d7debd5ec52d0d1fab4326c8e
anttont <anttoni.tornikoski@gmail.com>
2024-04-02 01:48:20 +0300

    Blogs added

--- src/containers/blog/blog.jsx
+++ src/containers/blog/blog.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import "./blog.css"
+import { Article } from '../../components'
+import { blog01, blog02, blog03, blog04, blog05} from "./imports"
 
 const Blog = () => {
   return (
-    <div>
-      Blog
-    </div>
+    <div className='gpt3__blog section__padding' id="blog">
+      <div>
+        <h1 className='gradient__text'>A lot is happening</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date = "Sep 28, 2032" title="A sandstorm moves from the direction of the large desert. "/>
+          </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imgUrl={blog02} date = "Sep 28, 2032" title="Build and Deploy 4 Modern React Apps and Get Hired as a Frontend Developer | Full 10-Hour Course"/>
+          <Article imgUrl={blog03} date = "Sep 28, 2032" title="Build and Deploy 4 Modern React Apps and Get Hired as a Frontend Developer | Full 10-Hour Course"/> 
+          <Article imgUrl={blog04} date = "Sep 28, 2032" title="Build and Deploy 4 Modern React Apps and Get Hired as a Frontend Developer | Full 10-Hour Course"/> 
+          <Article imgUrl={blog05} date = "Sep 28, 2032" title="Build and Deploy 4 Modern React Apps and Get Hired as a Frontend Developer | Full 10-Hour Course"/>
+            </div>
+          </div>
+        </div>
+      
   )
 }
 
